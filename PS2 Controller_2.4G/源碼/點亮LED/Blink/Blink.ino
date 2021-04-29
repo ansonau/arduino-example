a862c5a33487bdec2233a00a7a626722348e2489
ansonau <ash1712@gmail.com>
2021-04-29 11:30:45 +0800

    update

--- PS2 Controller_2.4G/源碼/點亮LED/Blink/Blink.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  pinMode(13, OUTPUT);//设定数字13管脚为输出
-}
-void loop() {
-  digitalWrite(13, HIGH); //数字13管脚输出高电平
-  delay(1000); //延时1000ms                      
-  digitalWrite(13, LOW);//数字13管脚输出低电平
-  delay(1000); //延时1000ms
-}
